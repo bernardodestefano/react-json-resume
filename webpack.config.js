8140209a352d59b0913cc676be82428b1e2bbde6
Bernardo DeStefano <bernardo.destefano@EF.com>
2018-01-15 10:45:39 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./dist/index.html"
+    })
+  ]
+};
