79b4938f1d66f2d060551655f828190c1c382da1
bernardo de stefano <bernardo.destefano@gmail.com>
2018-01-25 15:06:17 +0100

    switch to vanilla react form, no redux-form anymore

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,45 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Basics from './forms/Basics';
+import Skills from './forms/Skills';
 
-export default class App extends Component {
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        basics: {
+          name: '',
+          role: '',
+          picture: '',
+          email: '',
+          phone: '',
+          website: '',
+          summary: ''
+        },
+        skills: {
+          name: '',
+          level: '',
+          keywords: []
+        }
+
+      }
+
+      this.handleForms = this.handleForms.bind(this);
+    }
+
+    handleForms(data,type) {
+      this.setState({[type]: data});
+      setTimeout(()=>console.log(this.state),200);
+    }
+
     render () {
-        return <p>This is my new react app</p>
+      return (
+        <div className="App">
+          <Basics basics={this.state.basics} onSubmit={this.handleForms}/>
+          <Skills skills={this.state.skills} onSubmit={this.handleForms}/>
+        </div>
+      )
     }
 }
+
+export default App;
